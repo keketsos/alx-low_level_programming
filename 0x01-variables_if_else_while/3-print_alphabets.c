adb924fc74ba67a1dc8db6dcc63ad99aaf2cc126
keketsos <ksemenya@icloud.com>
2023-06-17 13:15:53 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,18 +2,18 @@
 /**
  * main - Entry point
  * Return: Always 0 (success)
- */ 
+*/
 
 int main(void)
-{       
-        char c;
-        
-        c = 'a';
-        while
-                (c <= 'z') {
-		       putchar(c);
-	               c++;
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
 		}
-        putchar('\n');
-	return(0);
-} 
+	putchar('\n');
+	return (0);
+}
