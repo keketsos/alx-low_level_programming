d585f6a57aaf756675f035bf7bf323f599d2496c
keketsos <ksemenya@icloud.com>
2023-06-17 21:07:25 -0700

    task10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,38 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - function to print combination of two numbers
- *
+ * main - Entry point
  * Return: 0 on success
 */
 
 int main(void)
-{
-	int i = '0';
-	int j = '0';
 
-	while (i <= '9')
+{
+	int d, p;
+
+	for (d = '0'; d < '9'; d++)
 	{
-		while (j <= '9')
-		{
-			if (!(i > j) || i == j)
-			{
-				putchar(i);
-				putchar(j);
-				if (i == '8' && j == '9')
-				{
-					putchar('\n');
-				}
-				else
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-			j++;
-		}
-		j = '0';
-		i++;
-	}
-return (0);
-}
+	
+	for (p = d + 1; p <= '9'; p++)
+	return (0);
+{
