bf2b1162204db29aeca348bfba4d910674a64055
keketsos <ksemenya@icloud.com>
2023-06-17 18:46:14 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,23 +6,18 @@
 */
 
 int main(void)
-        {
-
+{
 	int a;
 
-	for (a =0; a <10; a++)
-        {
+	for (a =0; a < 10; a++)
+       	{
 		putchar(a + '0');
 		if (a < 9)
 		{
 			putchar(',');
-			putchar(',');
+			putchar(' ');
 		}
-        }
+	}      	
         putchar('\n');
-        return (0);
-        }
-
-
-
-
+	return (0);
+}
