cb8ad72be41e7eac9377b5906f4deca360df7931
keketsos <ksemenya@icloud.com>
2023-06-22 04:16:32 -0700

    isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_supper - check if a letter is upper
+ *@x: The number to be checked
+ *Return: 1 for upper letter or 0 for any else
+*/
+
+int _super(int x);
+{
+	if (x >= 65 && x <= 90)
+	{
+	return (0);
+	}
+	return (0);
+}
