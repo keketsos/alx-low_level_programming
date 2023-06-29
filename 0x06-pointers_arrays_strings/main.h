cc2cb201e8e680f3c566d0833ff2800257af27c1
keketsos <ksemenya@icloud.com>
2023-06-29 04:02:36 -0700

    Write a function that concatenates two strings.

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,3 @@
-
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -16,3 +15,4 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
+#endif
